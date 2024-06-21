3a345503a70081358a14c43b6237c633f1cd4823
Akki <166021923+aakki16@users.noreply.github.com>
2024-06-21 11:12:14 +0530

    Update blnkoo.js

--- dist/js/blnkoo.js
+++ dist/js/blnkoo.js
@@ -1,1 +1,19 @@
+/*!
+ * Blnkoo  v1.1.0 (https://blnkoo.com/)
+ * Copyright 2024-2024 The Blnkoo Authors
+ * Licensed under MIT (https://github.com/aakki16/blnkoo/LICENSE)
+*/
 
+/** Alert **/
+document.addEventListener('DOMContentLoaded', function(){
+  var closeButtons = document.querySelectorAll('.alert-close');
+    closeButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var alert = this.parentElement;
+        alert.style.opacity = '0';
+        setTimeout(function() {
+          alert.style.display = 'none';
+        }, 300);
+      });
+    });
+});
